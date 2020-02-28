6aefd84eaa6accbc2ca5be83cc7338766f480839
Gerrit Großkopf <gerrit@familiegrosskopf.de>
2020-02-28 14:26:31 +0100

    bug fixes
    almost done, works on windows, only misses textures and people waving

--- Assets/websockethandler.cs
+++ Assets/websockethandler.cs
@@ -21,7 +21,7 @@ public class websockethandler : MonoBehaviour
 {
 
     private MqttClient client;
-    string filename = "config/everything.conf";
+    string filename = "/everything.conf";
     public string topic_markers;
     public string topic_Temperature;
     public string topic_updatespeed;
@@ -32,9 +32,11 @@ public class websockethandler : MonoBehaviour
     public List<String> markers;
     public List<double> distances;
     public string firstmarker;
+    private string lastmarker="";
     public int marker_to_edit=0;
     public double max_accelleration=1.0;
     public TextMesh out_text;
+    private string _out_string;
     public InputField textfield_markerpos;
     public InputField textfield_speedTopic;
     public InputField textfield_stopTopic;
@@ -54,52 +56,73 @@ public class websockethandler : MonoBehaviour
     private float markertime;
     private double maxspeed;
     private double accelleration=0.0f;
-    private string username;
-    private string password;
-    private string email;
+    public string username;
+    public string password;
+    public string email;
+
     //Parse.
     
     //ClientWebSocket WebSocket;
     // Start is called before the first frame update
     void Start()
     {
+        markertime = DateTime.Now.Second;
+        filename = Application.persistentDataPath + filename;
+        movement = GetComponent<Move_along_path>();
+        Debug.Log("starting...");
         if (System.IO.File.Exists(filename))
         {
+            print("reader config");
             readConfig();
         }
         else
         {
+            print("writer config");
             writeOutConfig();
         }
+        //ReConnectWS();
         //connectWSAsync();
     }
     void ReConnectWS()
     {
-        client = new MqttClient(IPAddress.Parse(websocket_url), websocket_port, false, null);
+        print("Connecting to "+websocket_url);
+        client = new MqttClient(websocket_url);// + ":" + websocket_port.ToString());//IPAddress.Parse(websocket_url), websocket_port, false,null);
 
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
         string clientId = Guid.NewGuid().ToString();
-        client.Connect(clientId,username,password);
-
-        client.Subscribe(new string[] { topic_markers }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-        client.Subscribe(new string[] { topic_Temperature }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-        client.Subscribe(new string[] { topic_stop }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        print("using username: " + username);
+        print("using password: " + password);
+        byte code=client.Connect(clientId,username.Trim(),password.Trim());
+        print("got code from login: "+code.ToString());
+        client.Subscribe(new string[] {"#"}, new byte[] {MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE});
+        //client.Subscribe(new string[] { topic_markers }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        //client.Subscribe(new string[] { topic_Temperature }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        //client.Subscribe(new string[] { topic_stop }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
 
     }
     void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
     {
 
-        Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
-        if (e.Topic.Contains(topic_markers))
+        print("At Topic: " + e.Topic);
+        string messagetext= System.Text.Encoding.UTF8.GetString(e.Message);
+        if (e.Topic.Trim()==topic_markers.Trim())
         {
-            if (!markers.Contains(e.Message.ToString()))//got a new Marker, add it to the list
+            //print("Received: (" + messagetext+")");
+            //print("got a marker");
+            int markernum = markers.FindIndex(x => x.StartsWith(messagetext));
+            //print("marker is number "+markernum.ToString());
+            if (markernum<0)//got a new Marker, add it to the list
             {
-                markers.Add(e.Message.ToString());
+                //print("adding this");
+                markers.Add(messagetext);
                 tagconfig.options[markers.Count - 1].text=markers[markers.Count - 1];
-            }else if (distances.Count == 0)//Got all Markers, update my gui, sort my markers
+                
+            }else if (markernum == 0 && distances.Count==0)//Got all Markers, update my gui, sort my markers
             {
-                if(markerfirst.options[0].text!="Option A")
+
+                //print("got all markers, first is: "+markerfirst.options[0].text);
+                if (markers.IndexOf(markerfirst.options[0].text)!=-1)
                 {
                     OnFirstTagChanged(markers.IndexOf(markerfirst.options[0].text));
                     for(int i = 1; i < markers.Count; i++)
@@ -112,43 +135,91 @@ public class websockethandler : MonoBehaviour
                         {
                             List<Dropdown.OptionData> option_new = new List<Dropdown.OptionData>();
                             option_new.Add(new Dropdown.OptionData(markers[i]));
+                            markerfirst.options.Clear();
                             markerfirst.AddOptions(option_new);
+                            markerfirst.RefreshShownValue();
                             List<Dropdown.OptionData> tag_new = new List<Dropdown.OptionData>();
                             tag_new.Add(new Dropdown.OptionData("Tag"+i.ToString()));
-                            markerfirst.AddOptions(tag_new);
+                            tagconfig.options.Clear();
+                            tagconfig.AddOptions(tag_new);
+                            markerfirst.RefreshShownValue();
+                        }
+                    }
+                }
+                else
+                {
+
+                    OnFirstTagChanged(0);
+                    for (int i = 1; i < markers.Count; i++)
+                    {
+                        if (markerfirst.options.Count > i)
+                        {
+                            markerfirst.options[i].text = markers[i];
+                        }
+                        else
+                        {
+                            List<Dropdown.OptionData> option_new = new List<Dropdown.OptionData>();
+                            option_new.Add(new Dropdown.OptionData(markers[i]));
+                            markerfirst.options.Clear();
+                            markerfirst.AddOptions(option_new);
+                            markerfirst.RefreshShownValue();
+                            List<Dropdown.OptionData> tag_new = new List<Dropdown.OptionData>();
+                            tag_new.Add(new Dropdown.OptionData("Tag" + i.ToString()));
+                            tagconfig.options.Clear();
+                            tagconfig.AddOptions(tag_new);
+                            tagconfig.RefreshShownValue();
                         }
                     }
                 }
                 for(int i=0;i<markers.Count;i++){
-                    distances.Add(movement.maxdistance / markers.Count);
+                    distances.Add(i* movement.maxdistance / markers.Count);
                 }
             }
-            if(markers.Contains(e.Message.ToString()))//update speeds and position of train
+            if(markernum>=0)//>update speeds and position of train
             {
-                int markernum = markers.FindIndex(x => x.StartsWith(e.Message.ToString())); ;
-                double markerpos = distances[markernum];
-                double last_markerpos = distances[distances.Count - 1];
-                if (markernum != 0)
+
+                //print("updating speeds and feeds");
+                markernum = markers.FindIndex(x => x.StartsWith(messagetext));
+                int oldmarkernum = markers.FindIndex(x => x.StartsWith(lastmarker));
+                //print("got markernum " + markernum);
+
+                if (distances.Count <= markernum)
                 {
-                    last_markerpos = distances[markernum - 1];
+                    markers = new List<string>();
+                    distances = new List<double>();
                 }
-                double distance = markerpos - last_markerpos;
-                if (markerpos < last_markerpos)
+                else
                 {
-                    distance = movement.maxdistance - last_markerpos + markerpos;
+
+                    double markerpos = distances[markernum];
+                    double last_markerpos = 0;
+                    if (oldmarkernum!=-1)
+                        last_markerpos = distances[oldmarkernum];
+                    //if (markernum != 0)
+                    //{
+                    //    last_markerpos = distances[markernum - 1];
+                    //}
+                    double distance = markerpos - last_markerpos;
+                    if (markerpos < last_markerpos)
+                    {
+                        distance = movement.maxdistance - last_markerpos + markerpos;
+                    }
+
+                    if (distance / ((float)DateTime.Now.Millisecond/1000 - markertime) > maxspeed)
+                    {
+                        maxspeed = distance / ((float)DateTime.Now.Millisecond / 1000 - markertime);
+                    }
+                    markertime = (float)DateTime.Now.Millisecond / 1000;
+                    movement.DistanceTraveled = markerpos;
+                    accelleration = max_accelleration;
                 }
-                
-                if (distance/(Time.realtimeSinceStartup - markertime)>maxspeed)
-                {
-                    maxspeed = distance / (Time.realtimeSinceStartup - markertime);
-                }
-                markertime = Time.realtimeSinceStartup;
-                movement.DistanceTraveled = markerpos;
+                lastmarker = messagetext;
             }
         }
-        if (e.Topic.Contains(topic_stop))
+        if (e.Topic.Trim() == topic_stop.Trim())
         {
-            if ((e.Message.ToString().Contains("steht"))){
+            print("stopping/starting");
+            if ((messagetext.Contains("steht"))){
                 movement.MoveSpeed = 0.0f;
                 accelleration = 0.0f;
             }
@@ -158,9 +229,12 @@ public class websockethandler : MonoBehaviour
             }
 
         }
-        if (e.Topic.Contains(topic_Temperature))
+        if (e.Topic.Trim() == topic_Temperature.Trim())
         {
-            out_text.text = e.Message.ToString()+"°C";
+            print("temperature set"+messagetext);
+            _out_string = messagetext + "°C";
+            //out_text.text = messagetext + "°C";
+            
         }
     }
 
@@ -180,7 +254,7 @@ public class websockethandler : MonoBehaviour
         {
             if (line.StartsWith("WS_URL:"))
             {
-                websocket_url = line.Substring(8).Split(new char[] { ':' })[0];
+                websocket_url = line.Substring(8).Split(new char[] { ':' })[0];//+":"+line.Substring(8).Split(new char[] { ':' })[1];
                 if (line.Substring(8).Split(new char[] { ':' }).Length > 1)
                 {
                     websocket_port = Int32.Parse(line.Substring(7).Split(new char[] { ':' })[1]);
@@ -190,39 +264,39 @@ public class websockethandler : MonoBehaviour
             }
             else if (line.StartsWith("markertopic:"))
             {
-                topic_markers = line.Substring(13);
+                topic_markers = line.Substring("markertopic: ".Length);
             }
             else if (line.StartsWith("temptopic:"))
             {
-                topic_Temperature = line.Substring(12);
+                topic_Temperature = line.Substring("temptopic: ".Length);
             }
             else if (line.StartsWith("speedtopic:"))
             {
-                topic_updatespeed = line.Substring(13);
+                topic_updatespeed = line.Substring("speedtopic: ".Length);
             }
             else if (line.StartsWith("stoptopic:"))
             {
-                topic_stop = line.Substring(12);
+                topic_stop = line.Substring("stoptopic: ".Length);
             }
             else if (line.StartsWith("firstmarker:"))
             {
-                firstmarker = line.Substring(13);
+                firstmarker = line.Substring("firstmarker: ".Length);
             }
             else if (line.StartsWith("accelleration:"))
             {
-                accelleration = double.Parse(line.Substring(15));
+                accelleration = double.Parse(line.Substring("accelleration: ".Length));
             }
             else if (line.StartsWith("user:"))
             {
-                username = line.Substring(6);
+                username = line.Substring("user: ".Length);
             }
             else if (line.StartsWith("passwd:"))
             {
-                password = line.Substring(8);
+                password = line.Substring("passwd: ".Length);
             }
             else if (line.StartsWith("email:"))
             {
-                email = line.Substring(7);
+                email = line.Substring("email: ".Length);
             }
         }
         textfield_webSocket.text = websocket_url + ":" + websocket_port.ToString();
@@ -254,6 +328,17 @@ public class websockethandler : MonoBehaviour
         writer.WriteLine("passwd: " + password);
         writer.WriteLine("email: " + email);
         writer.Close();
+
+        textfield_webSocket.text = websocket_url + ":" + websocket_port.ToString();
+        textfield_tempTopic.text = topic_Temperature;
+        textfield_tagTopic.text = topic_markers;
+        textfield_stopTopic.text = topic_stop;
+        textfield_speedTopic.text = topic_updatespeed;
+        textfield_accelleration.text = accelleration.ToString();
+        textfield_username.text = username;
+        textfield_password.text = password;
+        textfield_email.text = email;
+        markerfirst.options[0].text = firstmarker;
     }
 
     //string temporarystorage = "";
@@ -261,6 +346,7 @@ public class websockethandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        out_text.text = _out_string;
         if (movement.MoveSpeed < maxspeed)
         {
             movement.MoveSpeed += accelleration * Time.deltaTime;
@@ -291,6 +377,7 @@ public class websockethandler : MonoBehaviour
     }
     public void OnFirstTagChanged(int toset)
     {
+        print("SetfirstTag: " + toset.ToString());
         if (toset != 0)
         {
             for (int i = 0; i < toset; i++)
@@ -352,6 +439,11 @@ public class websockethandler : MonoBehaviour
         username = toset;
         writeOutConfig();
     }
+    public void OnEmailChanged(string toset)
+    {
+        email = toset;
+        writeOutConfig();
+    }
     public void OnPasswordChanged(string toset)
     {
         password = toset;
@@ -360,8 +452,8 @@ public class websockethandler : MonoBehaviour
     public void OnSpeedChanged(Single toset)
     {
         currentspeed = toset;
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://192.168.1.102/motor?direction=1&speed=" + (70 + toset).ToString() + "&mode=0");
-        request.GetResponse();
+        // HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://192.168.1.102/motor?direction=1&speed=" + (70 + toset).ToString() + "&mode=0");
+        //request.GetResponse();
         client.Publish(topic_updatespeed, System.Text.Encoding.UTF8.GetBytes("speed to: " + 70+toset), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
     }
 
